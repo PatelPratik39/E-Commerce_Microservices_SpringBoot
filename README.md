69497860b2732451fa32c3139f4f4dd7042096e4
PatelPratik39 <ptl.pratik717@gmail.com>
2025-01-17 02:05:41 -0600

    Config server is up and running

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # E-Commerce_Microservices_SpringBoot
+
+
+![Image1](path/to/image)
+
+[!image] (.img1)
+[!image] (.img2)
+[!image] (.img3)
---
+++ services/config-server/src/main/java/com/ecommerce/configserver/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.ecommerce.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+		System.err.println("Config Server is up and Running!!!! ✅");
+	}
+
+}
