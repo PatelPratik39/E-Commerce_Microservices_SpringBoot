e334d5eb7ef3cf3df67b68f032bfd521288c6958
PatelPratik39 <ptl.pratik717@gmail.com>
2025-01-18 01:49:29 -0600

    Force re-adding discovery service as a normal folder

---
+++ services/discovery/src/main/java/com/fullstck/discovery/DiscoveryApplication.java
@@ -0,0 +1,16 @@
+package com.fullstck.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+        System.err.println("Eureka Discovery server running at PORT-8761 successfully ✅");
+    }
+
+}
