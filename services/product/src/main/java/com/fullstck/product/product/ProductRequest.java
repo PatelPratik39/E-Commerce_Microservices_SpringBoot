94048ed5943d9e0404dbd8ab1173e50cacb985a7
PatelPratik39 <ptl.pratik717@gmail.com>
2025-01-18 20:39:52 -0600

    Product mapper

---
+++ services/product/src/main/java/com/fullstck/product/product/ProductRequest.java
@@ -0,0 +1,25 @@
+package com.fullstck.product.product;
+
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public record ProductRequest(
+
+        Integer id,
+        @NotNull(message = "Product Name is required")
+        String name,
+        @NotNull(message = "Product Description is required")
+        String description,
+        @Positive(message = "Available Quantity should be positive")
+        double availableQuantity,
+        @Positive(message = "Price should be positive")
+        BigDecimal price,
+        @NotNull(message = "Product Category is required")
+        Integer categoryId
+) {
+
+
+}
