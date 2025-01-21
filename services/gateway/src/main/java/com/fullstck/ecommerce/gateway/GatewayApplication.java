cad6c4565b9478283031f4b5d8215a89b6d5b829
PatelPratik39 <ptl.pratik717@gmail.com>
2025-01-21 01:49:30 -0600

    API Gateway configured

---
+++ services/gateway/src/main/java/com/fullstck/ecommerce/gateway/GatewayApplication.java
@@ -0,0 +1,14 @@
+package com.fullstck.ecommerce.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+		System.out.println("API Gateway started successfully ✅");
+	}
+
+}
